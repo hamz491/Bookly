380b6fe6415dcba8b77299d29c5ab922c2349184
Hamza_HZ <hamzahegazy669@gmail.com>
2023-12-12 15:51:45 +0200

    add getx and create splashView

---
+++ lib/Features/Splash/Preserntaion/view/SplashView.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/Features/Splash/Preserntaion/view/Widgets/SplashView_Body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashView_Body();
+  }
+}
---
+++ lib/Features/Splash/Preserntaion/view/Widgets/SplashView_Body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashView_Body extends StatelessWidget {
+  const SplashView_Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
